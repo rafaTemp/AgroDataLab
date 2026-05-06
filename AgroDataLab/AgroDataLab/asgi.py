25b022b8dd31c09bf8f40fdba4bed00d731b19fa
ErichLeonardo <erich.hamm21@gmail.com>
2026-05-06 20:43:39 +0200

    Configura app EnviroProRecord en proyecto AgroDataLab

--- AgroDataLab/AgroDataLab/asgi.py
+++ AgroDataLab/AgroDataLab/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nombre_proyecto.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AgroDataLab.settings')
 
 application = get_asgi_application()
