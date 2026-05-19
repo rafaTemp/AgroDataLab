1a466712b3a9227a8b258658f5c4946ebbe14e0e
ErichLeonardo <erich.hamm21@gmail.com>
2026-05-19 19:51:38 +0200

    Configurar static y migraciones

--- AgroDataLab/AgroDataLab/settings.py
+++ AgroDataLab/AgroDataLab/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 
@@ -124,6 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Authentication settings
 LOGIN_URL = 'login'
